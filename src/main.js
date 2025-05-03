c42b83748da253b1e79363384414467686f53480
harishrsk55 <harishrsk55@gmail.com>
2025-05-03 18:19:29 +0530

    main stuff

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let dropdown;
+
+window.onload = function() {
+    dropdown = document.getElementById('dropdown');
+    dropdown.style.display = "none";
+};
+
+function drop() {
+    dropdown.style.display = "block";
+}
+
+function drop1(){
+    dropdown.style.display = "none";
+    
+}
